88f9bccaf5bcee980e1ebaa04a1cb36db82a6c2b
Phony Lou <louqilin555@gmail.com>
2020-11-20 09:13:22 +0800

    reduce hard code of currency

--- src/main/scala/com/example/Dollar.scala
+++ src/main/scala/com/example/Dollar.scala
@@ -2,6 +2,6 @@ package com.example
 
 class Dollar(override protected val amount: Int, currency: String) extends Money(amount, currency) {
 
-  override def times(i: Int): Dollar = new Dollar(amount, "USD")
+  override def times(i: Int): Dollar = new Dollar(amount, currency)
 
 }
