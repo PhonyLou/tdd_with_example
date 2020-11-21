2e04b6a086c6d2c40193c789110c9945fa2adbe5
Phony Lou <louqilin555@gmail.com>
2020-11-21 13:24:33 +0800

    fix test

--- src/test/scala/com/example/CombinedSpec.scala
+++ src/test/scala/com/example/CombinedSpec.scala
@@ -8,33 +8,33 @@ class CombinedSpec extends AnyWordSpec with Matchers {
   "Equal Spec" should {
 
     "return true when compare two Dollar object with same amount" in {
-      val five: Dollar = new Dollar(5, "USD")
-      five shouldEqual new Dollar(5, "USD")
-      five should not equal new Dollar(6, "USD")
+      val five: Money = Money.dollar(5)
+      five shouldEqual Money.dollar(5)
+      five should not equal Money.dollar(6)
     }
 
     "return true when compare two Franc object with same amount" in {
-      val five: Franc = new Franc(5, "CHF")
+      val five: Money = Money.franc(5)
       five shouldEqual new Franc(5, "CHF" )
       five should not equal new Franc(6, "CHF")
     }
 
     "return false when compare given one is Dollar and another one is Franc" in {
-      new Franc(5, "CHF") equals new Dollar(5, "USD") shouldEqual false
+      Money.franc(5) equals Money.dollar(5) shouldEqual false
     }
   }
 
   "Multiplication Spec" should {
     "Do Dollar multiplication" in {
       val five: Money = Money.dollar(5)
-      five.times(2) equals new Dollar(10, "USD")
-      five.times(3) equals new Dollar(15, "USD")
+      five.times(2) shouldEqual Money.dollar(10)
+      five.times(3) shouldEqual Money.dollar(15)
     }
 
     "Do Franc multiplication" in {
       val five: Money = Money.franc(5)
-      five.times(2) equals new Franc(10, "CHF")
-      five.times(3) equals new Franc(15, "CHF")
+      five.times(2) shouldEqual Money.franc(10)
+      five.times(3) shouldEqual Money.franc(15)
     }
 
   }
